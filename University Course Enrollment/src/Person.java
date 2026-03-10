b5f216b8e9e5698ee0f8eb3a4cfe0314204d79c4
OmarFarouk-Code <omarrfaroukk1212@gmail.com>
2026-03-10 23:22:47 +0200

    added getters/setters and constructor for Person

--- University Course Enrollment/src/Person.java
+++ University Course Enrollment/src/Person.java
@@ -1,4 +1,45 @@
 public class Person
 {
+    private String email;
+    private String name;
+    private String ID;
 
+    public Person ( String email , String name , String ID )
+    {
+        this.email = email;
+        this.name = name;
+        this.ID = ID;
+    }
+
+    public String getEmail()
+    {
+        return email;
+    }
+
+    public void setEmail( String email )
+    {
+        this.email = email;
+    }
+
+    public String getName()
+    {
+        return name;
+    }
+
+    public void setName( String name )
+    {
+        this.name = name;
+    }
+
+    public String getID()
+    {
+        return ID;
+    }
+
+    public void setID( String ID )
+    {
+        this.ID = ID;
+    }
+
+    
 }
