d30ddc918eed071b79b37cb57f008e152338e719
OmarFarouk-Code <omarrfaroukk1212@gmail.com>
2026-03-11 00:05:45 +0200

    Fully Finished Main Class Hierarchy and main attributes aswell as some helper functions

---
+++ University Course Enrollment/src/EnrollmentSystem.java
@@ -0,0 +1,48 @@
+import java.util.ArrayList;
+
+public class EnrollmentSystem 
+{
+    private ArrayList <Student> allStudents;
+    private ArrayList <Course> allCourses;
+
+    public EnrollmentSystem()
+    {
+        this.allStudents = new ArrayList<>();
+        this.allCourses = new ArrayList<>();
+    }
+
+    public void registerStudent ( Student student )
+    {
+        allStudents.add(student);
+    }
+
+    public void addCourse ( Course course )
+    {
+        allCourses.add(course);
+    }
+
+    public Student findStudentById(String ID )
+    {
+        for ( Student student : allStudents )
+        {
+            if ( student.getID() == ID )
+            {
+                return student;
+            }
+        }
+        return null;
+    }
+
+    public Course findCourseByCode(String code)
+    {
+        for ( Course course : allCourses )
+        {
+            if ( course.getCourseCode() == code )
+            {
+                return course;
+            }
+        }
+        return null;
+    }
+
+}
